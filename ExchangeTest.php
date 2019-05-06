6e2ae28e3b636d93753ab6529122b5ba22ec90eb
Leo Wattier <leo.wattier@free.fr>
2019-05-06 17:03:07 +0200

    testDate exchange

--- ExchangeTest.php
+++ ExchangeTest.php
@@ -7,16 +7,6 @@ use PHPUnit\Framework\TestCase;
 
 class ExchangeTest extends TestCase {
 
-    // public function testSave(){
-        
-    //     $exchange = $this->createMock(Exchange::class);
-
-    //     $exchange->method('save')
-    //         ->willReturn(1);
-
-    //     $this->assertEquals(1, $exchange->save());
-    // } 
-
     public function testIsValid()
 	{
         $receiver = $this->getMockBuilder(User::class)->disableOriginalConstructor()->getMock();
@@ -32,7 +22,26 @@ class ExchangeTest extends TestCase {
         $product->method('isValid')
         ->willReturn(1);
 
-		$exchange = new Exchange($receiver,$product,$owner,"2019-05-15","2019-06-27");
-		$this->assertEquals(1, $exchange->isValid());
-	}	
+		$exchange = new Exchange($receiver,$product,$owner,"2019-05-15","2019-06-17");
+		$this->assertEquals(1, $exchange->isValid(), "Echange invalide");
+    }	
+    
+    public function testInvalidDate(){
+
+        $receiver = $this->getMockBuilder(User::class)->disableOriginalConstructor()->getMock();
+        $owner = $this->getMockBuilder(User::class)->disableOriginalConstructor()->getMock();
+        $product = $this->getMockBuilder(Product::class)->disableOriginalConstructor()->getMock();
+
+        $receiver->method('isValid')
+        ->willReturn(1);
+
+        $owner->method('isValid')
+        ->willReturn(1);
+
+        $product->method('isValid')
+        ->willReturn(1);
+
+        $exchange = new Exchange($receiver,$product,$owner,"2019-05-15","2019-06-17");
+        $this->assertEquals(1, $exchange->isValid(), "Date invalide");
+    }
 }
