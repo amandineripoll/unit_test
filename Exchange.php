2b3e4053be190996cd0f00c484fd3f9c40a2c434
Leo Wattier <leo.wattier@free.fr>
2019-05-07 11:19:37 +0200

    email send if minor

--- Exchange.php
+++ Exchange.php
@@ -51,24 +51,22 @@ class Exchange extends Product {
         }
     }
 
-   public function save(){
-    if($this->isValid()){
-        try{
-            DBConnection::saveExchange($this);
-        }
-        catch(Exception $e){
-            echo $e;
-        }
+    public function save(){
+        if($this->isValid() && $this->receiver->age >= 18){
+            try{
+                DBConnection::saveExchange($this);
+            }
+            catch(Exception $e){
+                echo $e;
+            }
+        } else {
+            try{
+                $message = "Veuillez renseigner les informations complémentaires indiquées en pièce jointe";
+                EmailSender::sendEmail($this->receiver->email, $message);
+            }
+            catch(Exception $e){
+                echo $e;
+            }
         }
     }
 }
-
-// $userOwner = new User("dd", "dd", "amadd@dmail.r", 15);
-// $userReceiver = new User("aa", "aa", "amadd@dmail.r", 25);
-// $product = new Product("chaise", $userOwner);
-// $end = "2019-06-27";
-// $begin = "2019-05-15";
-// $exchange = new Exchange($userReceiver, $product, $userOwner, $begin, $end);
-// print $exchange->save();
-// && $this->BeginningDate < $this->EndingDate
-//(new DateTime()->format('Y-m-d') < $this->BeginningDate)q
